0b429a730e18184508cf292087b69eade4479a29
CK <chamkank@users.noreply.github.com>
2018-07-03 22:57:12 -0700

    fix syntax

--- serverless/functions/example/index.js
+++ serverless/functions/example/index.js
@@ -2,6 +2,6 @@
 * @param {Object} req Cloud Function request context.
 * @param {Object} res Cloud Function response context.
 */
-export function example(req, res) {
+exports.example = (req, res) => {
   res.send('Hello World!');
-} 
+}
