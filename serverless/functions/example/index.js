f4c00900c8b9c47762d952c057ba371f422789fb
CK <chamkank@users.noreply.github.com>
2018-07-03 22:50:10 -0700

    add example function

---
+++ serverless/functions/example/index.js
@@ -0,0 +1,7 @@
+/**
+* @param {Object} req Cloud Function request context.
+* @param {Object} res Cloud Function response context.
+*/
+export function example(req, res) {
+  res.send('Hello World!');
+} 
