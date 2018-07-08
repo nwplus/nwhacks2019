24f22e97c53d8718a861d1883676a93a612381c0
CK <chamkank@users.noreply.github.com>
2018-07-07 20:26:32 -0700

    Refactor test to use main.config.js

--- web/tests/firebase/firebase.js
+++ web/tests/firebase/firebase.js
@@ -1,25 +1,7 @@
+const config = require('../../main.config').firebaseConfig;
 const firebase = require('firebase');
 // Required for side-effects
 require('firebase/firestore');
-
-const prodConfig = {
-  // TODO: setup production firestore project
-};
-
-const devConfig = {
-  apiKey: 'AIzaSyCBkQHeikIsiYZ2yOHiqH_mGJKDWMDU500',
-  authDomain: 'nwhacks-2019.firebaseapp.com',
-  databaseURL: 'https://nwhacks-2019.firebaseio.com',
-  projectId: 'nwhacks-2019',
-  storageBucket: 'nwhacks-2019.appspot.com',
-  messagingSenderId: '98283589440',
-};
-
-const config = {
-  production: prodConfig,
-  development: devConfig,
-};
-
 firebase.initializeApp(config[process.env.NODE_ENV]);
 
 const auth = firebase.auth();
