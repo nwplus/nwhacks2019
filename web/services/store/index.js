11b8167d668d52b1cf83be451f1faf5c415b2042
CK <chamkank@users.noreply.github.com>
2018-07-07 19:15:20 -0700

    Refactor store to use main.config.js

--- web/services/store/index.js
+++ web/services/store/index.js
@@ -6,30 +6,7 @@ import 'firebase/database';
 import { persistStore, persistReducer } from 'redux-persist';
 import localStorage from 'redux-persist/lib/storage';
 import hardSet from 'redux-persist/lib/stateReconciler/hardSet';
-
-const prodConfig = {
-  // TODO: setup production firestore project
-};
-
-const devConfig = {
-  apiKey: 'AIzaSyCBkQHeikIsiYZ2yOHiqH_mGJKDWMDU500',
-  authDomain: 'nwhacks-2019.firebaseapp.com',
-  databaseURL: 'https://nwhacks-2019.firebaseio.com',
-  projectId: 'nwhacks-2019',
-  storageBucket: 'nwhacks-2019.appspot.com',
-  messagingSenderId: '98283589440',
-};
-
-const config = {
-  production: prodConfig,
-  development: devConfig,
-};
-
-// react-redux-firebase config
-const rrfConfig = {
-  // userProfile: 'users',
-  // useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
-};
+import { firebaseConfig, rrfConfig } from "../../main.config";
 
 const persistConfig = {
   key: 'root',
@@ -38,7 +15,7 @@ const persistConfig = {
 
 export default (initialState = {}) => {
   // Initialize firebase instance
-  firebase.initializeApp(config[process.env.NODE_ENV]);
+  firebase.initializeApp(firebaseConfig[process.env.NODE_ENV]);
 
   // Add reactReduxFirebase store enhancer when making store creator
   const createStoreWithFirebase =
