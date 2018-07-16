a3416323f1356f03114c2eb5bc0f810f6da4668c
CK <chamkank@users.noreply.github.com>
2018-07-15 22:25:53 -0700

    Debugging

--- serverless/index.js
+++ serverless/index.js
@@ -1,10 +1,8 @@
 const exampleFunction = require('./src/example');
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+// const functions = require('firebase-functions');
+// const admin = require('firebase-admin');
 
-admin.initializeApp(functions.config().firebase); 
-const database = admin.database();
+// admin.initializeApp(functions.config().firebase); 
+// const database = admin.database();
 
-exports.example = functions.https.onRequest((req, res) => {
-    exampleFunction.handler(req, res);
-});
+exports.example = exampleFunction.handler(req, res);
