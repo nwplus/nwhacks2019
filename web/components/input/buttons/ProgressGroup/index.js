1ce9f46fe20d2bc78e1c3e3136f4c0f70aa39ff5
Robert Lin <robertlin1@gmail.com>
2018-07-16 20:33:55 -0700

    Update ProgressRoute with simpler API

--- web/components/input/buttons/ProgressGroup/index.js
+++ web/components/input/buttons/ProgressGroup/index.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ProgressGroup = ({ activeIndex, steps }) => steps.map((s) => {
-  const active = (activeIndex === 0);
-  activeIndex -= 1;
-  return (
-    <span key={s.text}>
-      <button
-        onClick={s.onClick}
-        type="button"
-        className={`
-          progress-step ${active ? 'active' : ''} ${s.disabled ? 'disabled' : ''}
-        `}>
-        { s.text }
-      </button>
-    </span>
-  );
-});
+const ProgressGroup = ({ count, onClick, activeIndex, lastValidIndex }) => {
+  const buttons = [];
+  for (let i = 0; i < count; i += 1) {
+    const active = (i === activeIndex);
+    buttons.push(
+      <span key={i}>
+        <button
+          onClick={onClick}
+          type="button"
+          className={`
+            progress-step
+            ${active ? 'active' : ''} 
+            ${i > lastValidIndex ? 'disabled' : ''}
+          `}>
+          { i + 1 }
+        </button>
+      </span>
+    );
+  }
+  return buttons;
+};
 
 ProgressGroup.propTypes = {
-  activeIndex: PropTypes.number,
-  steps: PropTypes.arrayOf(PropTypes.shape({
-    text: PropTypes.string.isRequired,
-    onClick: PropTypes.func,
-    disabled: PropTypes.bool,
-  })),
+  count: PropTypes.number.isRequired, // number of elements to create
+  onClick: PropTypes.func, // assigned to buttons based on index
+  activeIndex: PropTypes.number, // currently "clicked"
+  lastValidIndex: PropTypes.number, // all values after this will be "disabled"
 };
 
 export default ProgressGroup;
