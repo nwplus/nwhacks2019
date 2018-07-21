1ec6ac654a2228f14f669b0cc8b74ab1c0cb6ece
CK <chamkank@users.noreply.github.com>
2018-07-20 23:21:26 -0700

    Use index.js for exporting functions only
    We will define functions seperately in functions/src/. Each file in src will be responsible for importing their own dependencies.
    
    Also note that each file can export more than one function so we can group related functions into the same files (ex: email.js)

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,7 @@
-const functions = require('firebase-functions');
+// Handles exporting functions to GCF
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// import functions
+const exampleFunction = require('./src/example').helloWorld;
+
+// export functions
+exports.example = exampleFunction;
