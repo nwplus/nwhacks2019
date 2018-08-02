e9f945f1479a857737941f0d806be374c79f523b
CK <chamkank@users.noreply.github.com>
2018-08-01 17:51:37 -0700

    Allow Checkbox to render without CheckboxGroup

--- web/components/input/buttons/CheckboxGroup/index.js
+++ web/components/input/buttons/CheckboxGroup/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const InputContext = React.createContext();
+const InputContext = React.createContext({});
 
 const Checkbox = ({ name, value, label, checked, disabled, onChange }) => (
   <InputContext.Consumer>
