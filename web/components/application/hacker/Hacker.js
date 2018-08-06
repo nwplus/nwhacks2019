7912625792763e6572526ea6a51a1ae0d799c126
Min Gyo Kim <mingyo.kim3@gmail.com>
2018-08-06 13:24:00 -0700

    mobile responsive

--- web/components/application/hacker/Hacker.js
+++ web/components/application/hacker/Hacker.js
@@ -11,23 +11,26 @@ const HackerApplication = (props) => {
 
   const { count, activeIndex, lastValidIndex, onPageChange, onPageBack, onPageNext } = props;
   return (
-    <div className="below-nav">
-      <p>o hello there pls apply</p>
-      <ProgressGroup
-        count={count}
-        activeIndex={activeIndex}
-        lastValidIndex={lastValidIndex}
-        onClick={onPageChange}
-        />
-      <SecondaryButton
-        text="Back"
-        onClick={onPageBack}
-        disabled={activeIndex == 0}
-        />
-      <PrimaryButton
-        text="Next"
-        onClick={onPageNext}
-        />
+    <div className="below-nav" id="hacker-application">
+      <div>
+        <ProgressGroup
+          count={count}
+          activeIndex={activeIndex}
+          lastValidIndex={lastValidIndex}
+          onClick={onPageChange}
+          />
+        <h1>About you</h1>
+        <p>nwHacks is Western Canada’s largest collegiate hackathon taking place on January 26th - 27th, 2019 at the University of British Columbia. As expected, we receive a large number of applications every year. We focus on curating a quality hackathon experience for each attendee. For a fair assessment of your application, we encourage you to put your best foot forward on this journey!⛰️</p>
+        <SecondaryButton
+          text="Back"
+          onClick={onPageBack}
+          disabled={activeIndex === 0}
+          />
+        <PrimaryButton
+          text="Next"
+          onClick={onPageNext}
+          />
+      </div>
     </div>
   );
 };
