18eb018929d8223b6dc171ef78e2ae419fc5a293
Robert Lin <robertlin1@gmail.com>
2018-08-09 01:31:26 -0700

    Animate navbar based on scroll

--- web/components/navbar/index.js
+++ web/components/navbar/index.js
@@ -24,49 +24,69 @@ const getButton = (buttonType) => {
   }
 };
 
-const Navbar = ({ displayType, buttonType }) => {
-  const logoDiv = (
-    <div className="flex ai-center jc-start margin-sides">
-      <div className="flex ai-center">{getLogo()}</div>
-    </div>
-  );
-  const button = getButton(buttonType);
-  const links = [
-    <Link to="/"><b>About</b></Link>,
-    <Link to="/"><b>Stories</b></Link>,
-    <Link to="/"><b>FAQ</b></Link>,
-    <Link to="/"><b>Sponsors</b></Link>,
-    <Link to="/"><b>2018</b></Link>,
-    button,
-  ];
-
-  let linksDiv;
-  let key = 0;
-  switch (displayType) {
-    case DISPLAY_TYPE.ONLY_LOGO:
-      linksDiv = logoDiv;
-      break;
-    case DISPLAY_TYPE.LOGO_AND_BUTTON:
-      linksDiv = <div className="flex ai-center">{button}</div>;
-      break;
-    default:
-      linksDiv = links.map(l => (
-        <div
-          key={key += 1}
-          className="flex ai-center margin-sides-large">
-          {l}
-        </div>
-      ));
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    const { displayType, buttonType } = props;
+    this.state = { displayType, buttonType, collapsed: true };
   }
-  return (
-    <nav className="fill-width shadow flex">
-      {logoDiv}
-      <div className="flex jc-end margin-horizontal-divs fill-width">
-        {linksDiv}
-      </div>
-    </nav>
-  );
-};
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll, { passive: true });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = () => {
+    const top = window.pageYOffset || document.documentElement.scrollTop;
+    this.setState({ collapsed: (top < 64) });
+  }
+
+  render() {
+    const { displayType, buttonType, collapsed } = this.state;
+    const button = getButton(buttonType);
+    const links = [
+      <Link to="/"><b>About</b></Link>,
+      <Link to="/"><b>Stories</b></Link>,
+      <Link to="/"><b>FAQ</b></Link>,
+      <Link to="/"><b>Sponsors</b></Link>,
+      <Link to="/"><b>2018</b></Link>,
+      button,
+    ];
+
+    let linksDiv;
+    let key = 0;
+    switch (displayType) {
+      case DISPLAY_TYPE.ONLY_LOGO:
+        break;
+      case DISPLAY_TYPE.LOGO_AND_BUTTON:
+        linksDiv = <div className="flex ai-center">{button}</div>;
+        break;
+      default:
+        console.log('generating');
+        linksDiv = links.map(l => (
+          <div
+            key={key += 1}
+            className="flex ai-center margin-sides-large">
+            {l}
+          </div>
+        ));
+    }
+
+    return (
+      <nav className={`fill-width flex ${collapsed ? 'collapsed' : 'shadow'}`}>
+        <div className="flex ai-center jc-start margin-sides">
+          <div className="flex ai-center">{getLogo()}</div>
+        </div>
+        <div className="flex jc-end margin-horizontal-divs fill-width">
+          {linksDiv}
+        </div>
+      </nav>
+    );
+  }
+}
 
 Navbar.propTypes = {
   displayType: PropTypes.symbol,
