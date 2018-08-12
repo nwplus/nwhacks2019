e05e2f55e23ea764300db1138b0c0e90a0f82a5a
CK <chamkank@users.noreply.github.com>
2018-08-11 20:04:05 -0700

    Add example className to demo

--- web/components/demo/index.js
+++ web/components/demo/index.js
@@ -57,7 +57,7 @@ class FrontEndComponents extends React.Component {
         </CheckboxGroup><br />
         <Checkbox label="Ungrouped Checkbox" value="value3" name="new_checkbox_name_2" />
         <br /><br />
-        <RadioGroup name="test_radio_group">
+        <RadioGroup name="test_radio_group" className="dir-col">
           <RadioButton label="Label 1" value="value1" selected />
           <RadioButton label="Label 2" value="value2" disabled />
           <RadioButton label="Label 3" value="value3" />
