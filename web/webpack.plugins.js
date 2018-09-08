74665350995a67698b70dc8c3e727995339d89ec
Robert Lin <robertlin1@gmail.com>
2018-09-08 00:26:40 -0700

    webpack: add BundleAnalyzerPlugin

--- web/webpack.plugins.js
+++ web/webpack.plugins.js
@@ -2,9 +2,11 @@ const webpack = require('webpack');
 const MinifyPlugin = require('babel-minify-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 // Define webpack plugins used in dev and prod configurations here
 module.exports = [
+  new BundleAnalyzerPlugin(),
   new webpack.EnvironmentPlugin(['NODE_ENV']),
   new HtmlWebpackPlugin({
     template: './index.html',
