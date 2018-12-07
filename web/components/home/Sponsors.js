0361d7f491a125ed2a0d3eeb33fcf3cfafa76c57
CK <chamkank@users.noreply.github.com>
2018-12-06 23:45:33 -0800

    Add more sponsors (#211)
    * Add deloitte, realtor, sticker mule, and wolfram to sponsors
    
    * make build

--- web/components/home/Sponsors.js
+++ web/components/home/Sponsors.js
@@ -9,7 +9,11 @@ import hootsuite from '../../assets/sponsors/mega/hootsuite.png';
 import sap from '../../assets/sponsors/mega/sap.png';
 // kilo
 import google from '../../assets/sponsors/kilo/google.svg';
+import realtor from '../../assets/sponsors/kilo/realtor.png';
+import deloitte from '../../assets/sponsors/kilo/deloitte.png';
 // in-kind
+import wolfram from '../../assets/sponsors/in-kind/wolfram.png';
+import stickermule from '../../assets/sponsors/in-kind/stickermule.png';
 import davidsTea from '../../assets/sponsors/in-kind/davidstea.svg';
 import grounds from '../../assets/sponsors/in-kind/grounds.png';
 import sparkingIce from '../../assets/sponsors/in-kind/sparkling-ice.svg';
@@ -28,16 +32,28 @@ const getSponsor = (alt, src, href, className) => (
 const Sponsors = () => (
   <div className="sponsors">
     <h2 className="margin-bottom-m">Sponsors</h2>
+    {/* tetra */}
     {getSponsor('Microsoft', microsoft, 'https://www.microsoft.com/', 'tetra')}
-    {getSponsor('Standard Library', stdlib, 'https://stdlib.com/', 'tetra')}<br />
+    {getSponsor('Standard Library', stdlib, 'https://stdlib.com/', 'tetra')}
+    <br />
+    {/* mega */}
     {getSponsor('CSE (Communications Security Establishment)', cse, 'https://www.cse-cst.gc.ca/', 'mega')}
-    {getSponsor('Hootsuite', hootsuite, 'https://hootsuite.com/', 'mega')}<br />
+    {getSponsor('Hootsuite', hootsuite, 'https://hootsuite.com/', 'mega')}
     {getSponsor('SAP Concur', sap, 'https://www.concur.ca/sap/', 'mega')}
-    {getSponsor('Google', google, 'https://www.google.com', 'kilo')}<br />
-    {getSponsor('DAVIDsTEA', davidsTea, 'https://www.davidstea.com/', 'in-kind')}
+    <br />
+    {/* kilo */}
+    {getSponsor('Realtor', realtor, 'https://www.realtor.com/about/', 'kilo')}
+    {getSponsor('Google', google, 'https://www.google.com', 'kilo')}
+    {getSponsor('Deloitte', deloitte, 'https://www2.deloitte.com/ca/en.html', 'kilo')}
+    <br />
+    {/* in-kind */}
     {getSponsor('Grounds For Coffee', grounds, 'https://groundsforcoffee.ca/locations/', 'in-kind')}
-    {getSponsor('Sparkling Ice', sparkingIce, 'https://www.sparklingice.ca/', 'in-kind')}
+    {getSponsor('DAVIDsTEA', davidsTea, 'https://www.davidstea.com/', 'in-kind')}
+    {getSponsor('Sticker Mule', stickermule, 'https://hackp.ac/mlh-stickermule-hackathons', 'in-kind')}
+    <br />
+    {getSponsor('Wolfram Research', wolfram, 'https://www.wolfram.com/', 'in-kind')}
     {getSponsor('Zoho', zoho, 'https://www.zoho.com/', 'in-kind')}
+    {getSponsor('Sparkling Ice', sparkingIce, 'https://www.sparklingice.ca/', 'in-kind')}
   </div>
 );
 
